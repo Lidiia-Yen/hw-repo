eee8bfda831053bea9f813d37d86934b7607ad32
Lidiia Yen <yenlidiya@gmail.com>
2026-05-09 23:44:14 +0300

    hw of lesson3 v1

--- hw_lesson3/src/logical.js
+++ hw_lesson3/src/logical.js
@@ -1,26 +1,26 @@
 const num1 = 3;
 const num2 = 5;
-console.log(num1 > num2); // false
-console.log(num1 < num2); // true
+console.log(num1 > num2);
+console.log(num1 < num2);
 
 const str1 = 'one';
 const str2 = 'two';
 const str3 = '3';
-console.log(str1 > str2); // false
-console.log(str1 == str2); // true
+console.log(str1 > str2);
+console.log(str1 == str2);
 
-console.log(num1 == str3); // false
-console.log(num1 === str3); // false // false
+console.log(num1 == str3);
+console.log(num1 === str3);
 console.log(num1 !== str3);
 
 const boolT = true;
 const boolF = false;
 console.log(boolT && boolF); // true AND false =false
 console.log(boolT || boolF); // true OR false = true
-console.log(!boolT); // false
-console.log(!boolF); // true
+console.log(!boolT);
+console.log(!boolF);
 
 const age1 = null;
 const age2 = undefined;
 const myAge = age1 ?? age2 ?? 29;
-console.log(myAge); // 30
+console.log(myAge);
