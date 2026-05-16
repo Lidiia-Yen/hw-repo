a1af70aab429646514fbf2fe2e05ba2bb90c925c
Lidiia Yen <yenlidiya@gmail.com>
2026-05-17 00:24:09 +0300

    lesson5_hw edited again

--- hw_lesson5/src/arrow-functions.js
+++ hw_lesson5/src/arrow-functions.js
@@ -1,9 +1,1 @@
-export const sum = (some_array) => {
-    let sum = 0;
-    let i = 0;
-    do {
-        sum += some_array[i];
-        i++;
-    } while (i < some_array.length);
-    return sum;
-};
+export const sum = (some_array) => some_array.reduce((acc, num) => acc + num, 0);
